dcaa3f304e93c05b6397d6eab2ff03445c203808
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-08 22:42:33 +0500

    Install react-router-dom and create dummy routing in App component

---
+++ client/src/routes/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => (
+  <div>
+    <h2>THIS IS THE HOMEPAGE</h2>
+    <section id="section#1">SECTION#1</section>
+    <section id="section#2">SECTION#2</section>
+    <section id="section#3">SECTION#3</section>
+  </div>
+);
+
+export default HomePage;
