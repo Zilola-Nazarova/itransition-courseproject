26ab42340daac6811733939e284226986ae790ab
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-08 13:35:28 +0500

    Create asyncThunks and extraReducers to be able to retrieve items/collections from Back-End

--- client/src/redux/collections/collectionsSlice.js
+++ client/src/redux/collections/collectionsSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const COLLECTIONS_URL = 'http://localhost:5000/collections';
+
+export const getCollections = createAsyncThunk(
+  'collections/getCollections',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios.get(COLLECTIONS_URL);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
 
 const initialState = {
   value: [
@@ -14,6 +29,22 @@ export const collectionsSlice = createSlice({
   name: 'collections',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getCollections.pending, (state) => {
+        state.isLoading = true;
+        state.error = false;
+      })
+      .addCase(getCollections.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = false;
+        state.value = action.payload;
+      })
+      .addCase(getCollections.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export default collectionsSlice.reducer;
--- client/src/redux/items/itemsSlice.js
+++ client/src/redux/items/itemsSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const ITEMS_URL = 'http://localhost:5000/items';
+
+export const getItems = createAsyncThunk(
+  'items/getItems',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios.get(ITEMS_URL);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
 
 const initialState = {
   value: [
@@ -14,6 +29,22 @@ export const itemsSlice = createSlice({
   name: 'items',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getItems.pending, (state) => {
+        state.isLoading = true;
+        state.error = false;
+      })
+      .addCase(getItems.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = false;
+        state.value = action.payload;
+      })
+      .addCase(getItems.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export default itemsSlice.reducer;
