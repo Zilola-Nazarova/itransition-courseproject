ed0d9eb4b1a1886827f07b9cab534cc44b8e03f6
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-08 18:00:09 +0500

    Fix eslint errors on Back-End

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import postRoutes from './routes/post.js';
 import collectionRoutes from './routes/collection.js';
 import itemRoutes from './routes/item.js';
 
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/collections', collectionRoutes);
