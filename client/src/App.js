a7b9df2ace1aa0206a14bf6c24b85b275da3a31b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-09 20:12:54 +0500

    Create route for Users component, add a link to it in Navigation, ensure that the users are rendered

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
+// import { getUsers } from './redux/users/usersSlice';
 // import { getCollections } from './redux/collections/collectionsSlice';
 // import { getItems } from './redux/items/itemsSlice';
 
 import CollectionsPage from './routes/CollectionsPage';
 import ItemsPage from './routes/ItemsPage';
+import UsersPage from './routes/UsersPage';
 import HomePage from './routes/HomePage';
 import Layout from './routes/Layout';
 import NotFound from './routes/NotFound';
@@ -14,6 +16,7 @@ import NotFound from './routes/NotFound';
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
+    // dispatch(getUsers());
     // dispatch(getCollections());
     // dispatch(getItems());
   }, [dispatch]);
@@ -23,6 +26,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="users" element={<UsersPage />} />
           <Route path="collections" element={<CollectionsPage />} />
           <Route path="items" element={<ItemsPage />} />
         </Route>
