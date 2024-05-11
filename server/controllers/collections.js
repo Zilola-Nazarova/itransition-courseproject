77303c6cb0db5f3b33e446a3b1bd66f01f5f2d62
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-11 21:23:50 +0500

    BugFix (insight from Eslint) - change '_id' to 'id' since req.params returns 'id'

--- server/controllers/collections.js
+++ server/controllers/collections.js
@@ -35,7 +35,7 @@ export const updateCollection = async (req, res) => {
 
 export const deleteCollection = async (req, res) => {
   const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No collection with id ${_id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No collection with id ${id}`);
   try {
     await Collection.findByIdAndRemove(id);
     res.status(200).json({ message: `Collections with id ${id} has been removed` });
