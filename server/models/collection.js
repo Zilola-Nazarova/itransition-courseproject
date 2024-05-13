e67a21004ab40e9ebff1953c278af1723980ead6
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-14 01:10:02 +0500

    Update Collection model (on server) and all related components (on client) to include required attributes (title, text, category, image)

--- server/models/collection.js
+++ server/models/collection.js
@@ -1,9 +1,28 @@
 import mongoose from 'mongoose';
 
-const collectionSchema = mongoose.Schema({
-  title: String,
-  description: String
-});
+const collectionSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    category: [{
+      type: String,
+      enum: ['cat1', 'cat2', 'cat3', 'other'],
+      required: true
+    }],
+    image: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const Collection = mongoose.model('Collection', collectionSchema);
 
