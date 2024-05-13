50501252e957001891b1eaa7e44600f6f75af2b4
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-13 23:32:07 +0500

    Make Forms(Create User, Create Item, Create Collection) closable

--- client/src/routes/CollectionsPage.js
+++ client/src/routes/CollectionsPage.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import Collections from '../components/collections/Collections';
 
-const CollectionsPage = () => (
-  <>
-    <Collections />
-    <Link to="form">Create Collection</Link>
-    <Outlet />
-  </>
-);
+const CollectionsPage = () => {
+  const [openForm, setOpenForm] = useState(false);
+  return (
+    <>
+      <Collections />
+      {openForm
+        ? <Link to="/collections" onClick={() => setOpenForm(false)}>Close</Link>
+        : <Link to="form" onClick={() => setOpenForm(true)}>Create Collection</Link>}
+      <Outlet />
+    </>
+  );
+};
 
 export default CollectionsPage;
