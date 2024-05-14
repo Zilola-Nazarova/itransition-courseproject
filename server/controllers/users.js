6cf11c48cc7e1e1e51f2e8a14449b5c7e0d65d7c
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-14 19:50:16 +0500

    Remove users.length validation since we're already handling this case on Front-End

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,9 +6,6 @@ import Collection from '../models/collection.js';
 export const getUsers = async (req, res) => {
   try {
     const users = await User.find().select('-password').lean();
-    if (!users?.length) {
-      return res.status(400).json({ message: 'No users found' });
-    }
     res.status(200).json(users);
   } catch (error) {
     res.status(404).json({ message: error.message });
