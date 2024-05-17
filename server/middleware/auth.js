de8a2dd82de071750167fca67366f8f48ad9dd63
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-18 01:26:41 +0500

    BugFix - make sure the client provided the token type (to know how to decode it) and make sure the user exist in database. This will prevent deleted users with still valid tokens from making requests that require authentication

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
+import User from '../models/user.js';
 
 const auth = async (req, res, next) => {
   try {
@@ -17,11 +18,15 @@ const auth = async (req, res, next) => {
     } else if (type === 'Basic') {
       decodedData = jwt.verify(token, 'test');
       req.userId = decodedData.id;
+    } else {
+      return res.status(401).json({ message: 'Invalid token type provided. Use Basic or Bearer' });
     }
+    const user = await User.findById(req.userId);
+    if (!user) return res.status(401).json({ message: 'This user is not registered' });
     next();
   } catch (error) {
     console.log(error);
-    res.status(401).json({ message: 'You are not authorized to access this page!' });
+    res.status(401).json({ message: 'Authorization failed' });
   }
 };
 
