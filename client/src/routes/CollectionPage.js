4cf6974b7ee2c9a879fc284a3bc52b2ea1dbd28b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-18 05:52:48 +0500

    Update UserPage and CollectionPage to display the record details alongside with all nested records to make navigations short and easy

--- client/src/routes/CollectionPage.js
+++ client/src/routes/CollectionPage.js
@@ -1,5 +1,32 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import Items from '../components/items/Items';
+import { getCollectionItems } from '../redux/items/itemsSlice';
 import CollectionDetails from '../components/collections/CollectionDetails';
 
-const CollectionPage = () => <CollectionDetails />;
+const CollectionPage = () => {
+  const dispatch = useDispatch();
+  const { userId, collId } = useParams();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    console.log('GETTING COLLECTION ITEMS');
+    dispatch(getCollectionItems({ userId, collId }));
+  }, [dispatch, collId, userId]);
+
+  return (
+    <>
+      <h2>COLLECTION ITEMS PAGE</h2>
+      <CollectionDetails />
+      <Items />
+      {pathname === `/users/${userId}/collections/${collId}/items/form`
+        ? <Link to={`/users/${userId}/collections/${collId}/items`}>Close</Link>
+        : <Link to="form">Create Item</Link>}
+      <Outlet />
+    </>
+  );
+};
 
 export default CollectionPage;
--- client/src/routes/UserPage.js
+++ client/src/routes/UserPage.js
@@ -1,5 +1,32 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import UserDetails from '../components/users/UserDetails';
+import Collections from '../components/collections/Collections';
+import { getUserCollections } from '../redux/collections/collectionsSlice';
 
-const UserPage = () => <UserDetails />;
+const UserPage = () => {
+  const dispatch = useDispatch();
+  const { userId } = useParams();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    console.log('GETTING USERCOLLECTIONS');
+    dispatch(getUserCollections(userId));
+  }, [dispatch, userId]);
+
+  return (
+    <>
+      <h2>USER COLLECTIONS PAGE</h2>
+      <UserDetails />
+      <Collections />
+      {pathname === `/users/${userId}/collections/form`
+        ? <Link to={`/users/${userId}/collections`}>Close</Link>
+        : <Link to="form">Create Collection</Link>}
+      <Outlet />
+    </>
+  );
+};
 
 export default UserPage;
