58caedc8166304fd2186955e0a462ec7238a677c
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-19 08:32:15 +0500

    Create Comment and Like models with associations

---
+++ server/models/comment.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const commentSchema = mongoose.Schema(
+  {
+    item: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'Item field can not be empty'],
+      ref: 'Item'
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'Author field can not be empty'],
+      ref: 'User'
+    },
+    text: {
+      type: String,
+      required: [true, 'Text field can not be empty']
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
