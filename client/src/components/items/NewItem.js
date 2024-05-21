ea8dd2025ed8f85c5951a56711259d3cfb898868
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-21 08:20:48 +0500

    Add input for tags with autosuggest feature in NewItem component

--- client/src/components/items/NewItem.js
+++ client/src/components/items/NewItem.js
@@ -1,20 +1,46 @@
-import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import AutoSuggest from 'react-autosuggest';
 import { postItem } from '../../redux/items/itemsSlice';
+import { getTags } from '../../redux/tags/tagsSlice';
 
 const NewItem = () => {
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getTags());
+  }, [dispatch]);
+  const options = useSelector((state) => state.tags.value);
+  const lowerCaseOptions = options.map((tag) => (
+    { id: tag._id, tagname: tag.tagname.toLowerCase() }
+  ));
   const { userId, collId } = useParams();
   const emptyItemObj = { title: '', text: '' };
   const [itemData, setItemData] = useState(emptyItemObj);
-  const clear = () => setItemData(emptyItemObj);
+  const [tags, setTags] = useState([]);
+  const [value, setValue] = useState('');
+  const clear = () => {
+    setItemData(emptyItemObj);
+    setValue('');
+    setTags([]);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(postItem({ userId, collId, newItem: itemData }));
+    dispatch(postItem({ userId, collId, newItem: { ...itemData, tags } }));
     clear();
   };
-
+  const [suggestions, setSuggestions] = useState([]);
+  const getSuggestions = (value) => lowerCaseOptions.filter(
+    (tag) => tag.tagname.includes(value.trim().toLowerCase()),
+  );
+  const pushTag = (tag) => {
+    setTags([...tags, tag]);
+    setValue('');
+  };
+  const excludeTag = (toDelete) => {
+    setTags(tags.filter((tag) => tag !== toDelete));
+  };
   return (
     <div className="form">
       <h3>CREATE ITEM</h3>
@@ -31,6 +57,44 @@ const NewItem = () => {
           value={itemData.text}
           onChange={(e) => setItemData({ ...itemData, text: e.target.value })}
         />
+        <ul>
+          {tags.map((tag) => (
+            <li key={uuidv4()}>
+              {tag}
+              <button type="button" onClick={() => excludeTag(tag)}>X</button>
+            </li>
+          ))}
+        </ul>
+        <AutoSuggest
+          suggestions={suggestions}
+          onSuggestionsClearRequested={() => setSuggestions([])}
+          onSuggestionsFetchRequested={({ value }) => {
+            setValue(value);
+            setSuggestions(getSuggestions(value));
+          }}
+          onSuggestionSelected={
+            (_, { suggestionValue }) => {
+              pushTag(suggestionValue);
+            }
+          }
+          getSuggestionValue={(suggestion) => suggestion.tagname}
+          renderSuggestion={(suggestion) => <span>{suggestion.tagname}</span>}
+          inputProps={{
+            placeholder: 'Enter tags',
+            value,
+            onChange: (_, { newValue }) => {
+              setValue(newValue);
+            },
+            onKeyDown: (e) => {
+              if (e.key !== 'Enter') return;
+              if (!e.target.value.trim()) return;
+              e.preventDefault();
+              pushTag(e.target.value.trim());
+            },
+          }}
+          highlightFirstSuggestion
+        />
+        <button type="button" onClick={() => pushTag(value)}>Add tag</button>
         <button type="submit">Submit</button>
         <button type="button" onClick={clear}>Clear</button>
       </form>
