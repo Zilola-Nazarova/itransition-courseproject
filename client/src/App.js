d1f78767a43caba1cfaad2b9e4468792e77d5c7c
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-24 09:25:08 +0500

    BugFix - ensure we don't show Login form to authenticated users even if they try to access it through address bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import {
+  Routes, Route, BrowserRouter, Navigate,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import CollectionPage from './routes/CollectionPage';
 import ItemPage from './routes/ItemPage';
 import UsersPage from './routes/UsersPage';
@@ -9,27 +12,31 @@ import NotFound from './routes/NotFound';
 import Auth from './components/auth/Auth';
 import TagPage from './routes/TagPage';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="*" element={<NotFound />} />
-        <Route index element={<HomePage />} />
-        <Route path="tags/:tagId" element={<TagPage />} />
-        <Route path="auth" element={<Auth />} />
-        <Route path="users">
-          <Route index element={<UsersPage />} />
-          <Route path=":userId/collections">
-            <Route index element={<UserPage />} />
-            <Route path=":collId/items">
-              <Route index element={<CollectionPage />} />
-              <Route path=":itemId" element={<ItemPage />} />
+const App = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="*" element={<NotFound />} />
+          <Route index element={<HomePage />} />
+          <Route path="tags/:tagId" element={<TagPage />} />
+          <Route path="auth" element={user ? <Navigate to="/" /> : <Auth />} />
+          <Route path="users">
+            <Route index element={<UsersPage />} />
+            <Route path=":userId/collections">
+              <Route index element={<UserPage />} />
+              <Route path=":collId/items">
+                <Route index element={<CollectionPage />} />
+                <Route path=":itemId" element={<ItemPage />} />
+              </Route>
             </Route>
           </Route>
         </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
