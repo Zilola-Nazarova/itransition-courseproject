9ec0c8174d260f6ae3f67bf464c8f073aa037f0c
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-25 11:02:20 +0500

    Add pagination to TagPage to provide users comfortable navigation

--- client/src/routes/TagPage.js
+++ client/src/routes/TagPage.js
@@ -1,33 +1,44 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTagItems } from '../redux/tags/tagsSlice';
+import Paginated from '../components/Paginated';
+import Item from '../components/items/item/Item';
 
 const TagPage = () => {
   const dispatch = useDispatch();
   const { tagId } = useParams();
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get('page') || '1';
   useEffect(() => {
-    dispatch(getTagItems(tagId));
-  }, [dispatch, tagId]);
-  const { tagItems, isLoading, error } = useSelector((state) => state.tags);
+    if (page) {
+      dispatch(getTagItems({ tagId, page }));
+    }
+  }, [dispatch, tagId, page]);
+  const {
+    tagItems, numberOfPages, isLoading, error,
+  } = useSelector((state) => state.tags);
 
   return (
     <>
       <h2>TAG ITEMS PAGE</h2>
-      <h3>{`#${tagItems?.tagname}`}</h3>
       {error && <p>{error}</p>}
       {isLoading && <p>Loading...</p>}
-      {tagItems && tagItems.items.map((item) => (
-        <Link key={uuidv4()} to={`/users/${item.author}/collections/${item.coll}/items/${item._id}`}>
-          <p>{item._id}</p>
-          <p>{item.title}</p>
-          <p>{item.text}</p>
-          {item.tags.map((tag) => <span key={uuidv4()}>{tag.tagname}</span>)}
-        </Link>
-      ))}
-      {tagItems?.length === 0
+      {tagItems?.items?.length > 0 && (
+        <>
+          <h3>{`#${tagItems?.tag.tagname}`}</h3>
+          <Paginated
+            pageCount={numberOfPages}
+            page={page}
+            items={tagItems.items}
+            renderItem={(item) => <Item item={item} key={uuidv4()} />}
+          />
+        </>
+      )}
+      {tagItems?.items?.length === 0
         && <p>Oops! Seems there are no items with this tag.</p>}
     </>
   );
