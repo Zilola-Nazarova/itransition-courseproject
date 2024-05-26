7fcc4c963a9bc2925668fa25e085de776ad04fe4
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-05-26 16:00:13 +0500

    Make use of s3 helper functions in collections conrtoller to upload images to s3 and retrive image urls, test this feature on s3

--- server/controllers/collections.js
+++ server/controllers/collections.js
@@ -1,10 +1,21 @@
 import Collection from '../models/collection.js';
 import User from '../models/user.js';
 import Item from '../models/item.js';
+import crypto from 'crypto';
+import sharp from 'sharp';
+import { uploadFile, deleteFile, getObjectSignedUrl } from '../middleware/s3.js'
+
+const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
 
 export const getCollections = async (req, res) => {
   try {
     const collections = await Collection.find().lean();
+    if (!collections) return res.status(400).json({ message: 'Collections not found' });
+    for (let collection of collections) {
+      if (collection.image) {
+        collection.imageUrl = await getObjectSignedUrl(collection.image);
+      }
+    }
     res.status(200).json(collections);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -17,12 +28,15 @@ export const getUserCollections = async (req, res) => {
     const { page } = req.query;
     const LIMIT = 3;
     const startIndex = (Number(page) - 1) * LIMIT;
-    let collections = await Collection
-      .find({ author: userId })
-      .sort({ updatedAt: -1 });
+    let collections = await Collection.find({ author: userId }).sort({ updatedAt: -1 }).lean();
+    if (!collections) return res.status(400).json({ message: 'Collections not found' });
     const total = collections.length;
     collections = collections.slice(startIndex, startIndex + LIMIT);
-    if (!collections) return res.status(400).json({ message: 'Collections not found' });
+    for (let collection of collections) {
+      if (collection.image) {
+        collection.imageUrl = await getObjectSignedUrl(collection.image);
+      }
+    }
     res.status(200).json({ data: collections, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -34,6 +48,9 @@ export const getCollection = async (req, res) => {
     const { userId, collectionId } = req.params;
     const collection = await Collection.findOne({ _id: collectionId, author: userId }).exec();
     if (!collection) return res.status(400).json({ message: 'Collection not found' });
+    if (collection.image) {
+      collection.imageUrl = await getObjectSignedUrl(collection.image);
+    }
     res.status(200).json(collection);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -43,10 +60,17 @@ export const getCollection = async (req, res) => {
 export const createCollection = async (req, res) => {
   try {
     const { userId } = req.params;
-    const { title, text, category, image } = req.body;
+    const { title, text, category } = req.body;
+    const file = req.file;
+    let image;
     if (!title || !text || !category) return res.status(400).json({ message: 'All fields are required' });
     const author = await User.findById(userId);
     if (!author) return res.status(400).json({ message: 'User not found' });
+    if (file) {
+      const buffer = await sharp(file.buffer).resize({ height: 1080, width: 1080, fit: 'contain' }).toBuffer();
+      image = randomImageName();
+      await uploadFile(buffer, image, file.mimetype);
+    }
     const newCollection = await Collection.create({ title, text, category, image, author: userId });
     author.colls.push(newCollection._id);
     await author.save();
@@ -60,12 +84,20 @@ export const updateCollection = async (req, res) => {
   try {
     const { collectionId, userId } = req.params;
     const { title, text, category, image } = req.body;
-    if (!title || !text || !category || !image) {
+    const file = req.file;
+    let newImage;
+    if (!title || !text || !category) {
       return res.status(400).json({ message: 'All fields are required' });
+    }
+    if (file) {
+      if (image) await deleteFile(image);
+      const buffer = await sharp(file.buffer).resize({ height: 1080, width: 1080, fit: 'contain' }).toBuffer();
+      newImage = randomImageName();
+      await uploadFile(buffer, newImage, req.file.mimetype);
     }
     const updatedCollection = await Collection.findOneAndUpdate(
       { _id: collectionId, author: userId },
-      { title, text, category, image },
+      { title, text, category, image: newImage },
       { new: true }
     );
     if (!updatedCollection) return res.status(400).json({ message: 'Collection not found' });
@@ -83,8 +115,10 @@ export const deleteCollection = async (req, res) => {
     const author = await User.findById(userId).exec();
     author.colls = author.colls.filter((id) => id.toString() !== collectionId);
     await author.save();
-    const result = await Collection.findOneAndDelete({ _id: collectionId, author: userId });
-    if (!result) return res.status(400).json({ message: 'Collection not found' });
+    const collection = await Collection.findOne({ _id: collectionId, author: userId });
+    if (!collection) return res.status(400).json({ message: 'Collection not found' });
+    if (collection.image) await deleteFile(collection.image);
+    await collection.deleteOne();
     res.status(200).json({ message: `Collection with id ${collectionId} has been deleted`, _id: collectionId });
   } catch (error) {
     res.status(409).json({ message: error.message });
