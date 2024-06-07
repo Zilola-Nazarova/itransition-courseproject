c63c7be52d605002f61436e8dc1ad9b39eef7ab1
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-06-08 02:39:39 +0500

    Restyle Actions component: position error on filed like action, make like/comment icons the same size, line up icons and error alerts

--- client/src/components/items/Actions.js
+++ client/src/components/items/Actions.js
@@ -1,7 +1,8 @@
+import Container from 'react-bootstrap/esm/Container';
 import Alert from 'react-bootstrap/Alert';
 import Stack from 'react-bootstrap/Stack';
 import Toast from 'react-bootstrap/Toast';
-import { FaRegCommentAlt } from 'react-icons/fa';
+import { BsChatLeft } from 'react-icons/bs';
 import { useState } from 'react';
 import { useParams } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
@@ -14,7 +15,7 @@ const Actions = () => {
   const dispatch = useDispatch();
   const [showForm, setShowForm] = useState(false);
   const toggleShowForm = () => setShowForm(!showForm);
-  const { value, error } = useSelector((state) => state.likes);
+  const { value, error: likeFailed } = useSelector((state) => state.likes);
   const { userId, collId, itemId } = useParams();
   const { user } = useSelector((state) => state.auth);
   const [commentData, setCommentData] = useState({ text: '' });
@@ -39,15 +40,34 @@ const Actions = () => {
   };
 
   return (
-    <div>
-      {error && (
-        <Alert data-bs-theme="dark" variant="danger">
-          {error}
-        </Alert>
-      )}
-      <Stack direction="horizontal" bg="success">
-        <Likes handleLike={handleLike} currentLike={currentLike} />
-        <FaRegCommentAlt color="white" onClick={toggleShowForm} />
+    <Container className="actions p-0">
+      <Stack
+        direction="horizontal"
+        gap={4}
+        className="p-1"
+      >
+        <Stack
+          direction="horizontal"
+        >
+          <BsChatLeft
+            color="white"
+            className="me-4"
+            onClick={toggleShowForm}
+          />
+          <Likes
+            handleLike={handleLike}
+            currentLike={currentLike}
+          />
+        </Stack>
+        {likeFailed && (
+          <Alert
+            data-bs-theme="dark"
+            variant="danger"
+            className="p-2 m-0"
+          >
+            {likeFailed}
+          </Alert>
+        )}
       </Stack>
       <Toast show={showForm} onClose={toggleShowForm}>
         <NewComment
@@ -56,7 +76,7 @@ const Actions = () => {
           handleChange={(e) => setCommentData({ text: e.target.value })}
         />
       </Toast>
-    </div>
+    </Container>
   );
 };
 
