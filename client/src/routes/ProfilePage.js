8a595258ffb93fb707456b299daf6d7d0c70b650
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-06-11 08:41:04 +0500

    Add a section to the ProfilePage dedicated to Jira requests; list all requests of the current user with the link to Jira ticket

--- client/src/routes/ProfilePage.js
+++ client/src/routes/ProfilePage.js
@@ -1,9 +1,11 @@
+import Container from 'react-bootstrap/Container';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import UserDetails from '../components/users/UserDetails';
 import { getUser } from '../redux/users/usersSlice';
 import { getCategories } from '../redux/categories/categoriesSlice';
+import JiraRequests from '../components/profile/JiraRequests';
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
@@ -15,17 +17,17 @@ const ProfilePage = () => {
   }, [dispatch, user]);
 
   return (
-    <>
-      <section>
-        <h2>MY PROFILE</h2>
-        <UserDetails user={user.user} />
-      </section>
-      <section>
-        <Link to={`/users/${user.user._id}/collections`}>
-          <h2>MY COLLECTIONS</h2>
-        </Link>
-      </section>
-    </>
+    <Container
+      className="width-limit"
+      data-bs-theme="dark"
+    >
+      <h1 className="text-light mb-4">MY PROFILE</h1>
+      <UserDetails user={user.user} />
+      <Link to={`/users/${user.user._id}/collections`}>
+        <h2>MY COLLECTIONS</h2>
+      </Link>
+      <JiraRequests />
+    </Container>
   );
 };
 
